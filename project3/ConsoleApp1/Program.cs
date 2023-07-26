d8537e3f24f8a0377ccbbfc783b2c0ffd48006f2
Hasan Tayfur <hasantayfur69@hotmail.com>
2023-07-26 18:07:35 +0300

    project 3 added

---
+++ project3/ConsoleApp1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter size of array: ");
+        int arraySize = int.Parse(Console.ReadLine());
+        
+        while (arraySize <= 0)
+        {
+            Console.WriteLine("Invalid array size");
+            Console.Write("Enter size of array: ");
+            arraySize = int.Parse(Console.ReadLine());
+        }
+        
+        int[] array = new int[arraySize];
+
+        Console.WriteLine("Enter array elements:");
+        for (int i = 0; i < arraySize; i++)
+        {
+            Console.Write($"0 --> {i}: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.Write("Enter start index: ");
+        int startIndex = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter end index: ");
+        int endIndex = int.Parse(Console.ReadLine());
+
+        while (startIndex < 0 || startIndex >= arraySize || endIndex < 0 || endIndex >= arraySize)
+        {
+            Console.WriteLine("Invalid indexes");
+            Console.Write("Enter start index: ");
+            startIndex = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter end index: ");
+            endIndex = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"Sum of between ranges: {SumInRange(array, startIndex, endIndex)}");
+        Console.WriteLine($"Sum of before between ranges: {SumBeforeIndex(array, startIndex)}");
+        Console.WriteLine($"Sum of after between ranges: {SumAfterIndex(array, endIndex)}");
+    }
+
+    static int SumInRange(int[] arr, int start, int end)
+    {
+        int sumInRange = 0;
+        for (int i = start; i <= end; i++)
+        {
+            sumInRange += arr[i];
+        }
+        return sumInRange;
+    }
+
+    static int SumBeforeIndex(int[] arr, int index)
+    {
+        int sumBeforeIndex = 0;
+        for (int i = 0; i < index; i++)
+        {
+            sumBeforeIndex += arr[i];
+        }
+        return sumBeforeIndex;
+    }
+
+    static int SumAfterIndex(int[] arr, int index)
+    {
+        int sumAfterIndex = 0;
+        for (int i = index + 1; i < arr.Length; i++)
+        {
+            sumAfterIndex += arr[i];
+        }
+        return sumAfterIndex;
+    }
+}
---
+++ project3/ConsoleApp2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string text = Console.ReadLine();
+        
+        while (text == "" || text == null)
+        {
+            Console.WriteLine("Invalid text");
+            Console.Write("Enter a string: ");
+            text = Console.ReadLine();
+        }
+        
+        bool newOperation = false;
+        while(newOperation != true)
+        {
+            Console.WriteLine("Choose an operation: ");
+            Console.WriteLine("1 --> İşlem 1");
+            Console.WriteLine("2 --> İşlem 2");
+            Console.WriteLine("3 --> İşlem 3");
+            Console.WriteLine("4 --> Exit");
+            int choice = int.Parse(Console.ReadLine());
+            string searchText = "";
+            
+            switch (choice)
+            {
+                case 1:
+                    searchText = TakeSearchText();
+                    Console.WriteLine("-------------------------------");
+
+                    int index = -1;
+                    bool findStatus = false;
+                    while ((index = text.IndexOf(searchText, index + 1)) != -1)
+                    {
+                        Console.WriteLine($"'{searchText}' in {index}. indeks");
+                        findStatus = true;
+                    }
+
+                    if (findStatus != true)
+                    {
+                        Console.WriteLine($"'{searchText}' not found");
+                    } 
+                    break;
+
+                case 2:
+                    searchText = TakeSearchText();
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    newOperation = true; 
+                    break;
+                
+                default:
+                Console.WriteLine("Invalid operation");
+                break;
+            }
+        }
+    }
+
+    static string TakeSearchText()
+    {
+        Console.Write("Enter a string that you want to search: ");
+        string searchText = Console.ReadLine();
+        while (searchText == "" || searchText == null)
+        {
+            Console.WriteLine("Invalid text");
+            Console.Write("Enter a string that you want to search: ");
+            searchText = Console.ReadLine();
+        }
+
+        return searchText;
+    }
+
+    
+
+
+}
