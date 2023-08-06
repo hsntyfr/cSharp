f10b482fa7e5aec3713ac452562558b3602750bd
Hasan Tayfur <hasantayfur69@hotmail.com>
2023-08-06 14:03:48 +0300

    project 3 finish

--- project3/ConsoleApp1/Program.cs
+++ project3/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ class Program
         Console.Write("Enter size of array: ");
         int arraySize = int.Parse(Console.ReadLine());
         
+        //This while loop checks if the array size is valid
         while (arraySize <= 0)
         {
             Console.WriteLine("Invalid array size");
@@ -17,36 +18,60 @@ class Program
         int[] array = new int[arraySize];
 
         Console.WriteLine("Enter array elements:");
+        
+        //This for loop takes the array elements
         for (int i = 0; i < arraySize; i++)
         {
             Console.Write($"0 --> {i}: ");
             array[i] = int.Parse(Console.ReadLine());
         }
+        
+        bool newOperation = false;
 
-        Console.Write("Enter start index: ");
-        int startIndex = int.Parse(Console.ReadLine());
-
-        Console.Write("Enter end index: ");
-        int endIndex = int.Parse(Console.ReadLine());
-
-        while (startIndex < 0 || startIndex >= arraySize || endIndex < 0 || endIndex >= arraySize)
+        while (newOperation != true)
         {
-            Console.WriteLine("Invalid indexes");
-            Console.Write("Enter start index: ");
-            startIndex = int.Parse(Console.ReadLine());
+            Console.WriteLine("1 --> Select new indexes");
+            Console.WriteLine("2 --> Exit");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    WriteArray(array);
+                    Console.Write("Enter start index: ");
+                    int startIndex = int.Parse(Console.ReadLine());
 
-            Console.Write("Enter end index: ");
-            endIndex = int.Parse(Console.ReadLine());
+                    Console.Write("Enter end index: ");
+                    int endIndex = int.Parse(Console.ReadLine());
+
+                    //This while loop checks if the start and end indexes are valid
+                    while (startIndex < 0 || startIndex >= arraySize || endIndex < 0 || endIndex >= arraySize || startIndex > endIndex)
+                    {
+                        WriteArray(array);
+                        Console.WriteLine("Invalid indexes");
+                        Console.Write("Enter start index: ");
+                        startIndex = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter end index: ");
+                        endIndex = int.Parse(Console.ReadLine());
+                    }
+                    Console.WriteLine("-------------------------------");
+                    Console.WriteLine($"Sum of between ranges: {SumInRange(array, startIndex, endIndex)}");
+                    Console.WriteLine($"Sum of before between ranges: {SumBeforeIndex(array, startIndex)}");
+                    Console.WriteLine($"Sum of after between ranges: {SumAfterIndex(array, endIndex)}");
+                    break;
+                
+                case 2:
+                    newOperation = true;
+                    break;
+            }
         }
-
-        Console.WriteLine($"Sum of between ranges: {SumInRange(array, startIndex, endIndex)}");
-        Console.WriteLine($"Sum of before between ranges: {SumBeforeIndex(array, startIndex)}");
-        Console.WriteLine($"Sum of after between ranges: {SumAfterIndex(array, endIndex)}");
     }
-
+    
     static int SumInRange(int[] arr, int start, int end)
     {
         int sumInRange = 0;
+        
+        //This for loop sums the elements between the start and end indexes
         for (int i = start; i <= end; i++)
         {
             sumInRange += arr[i];
@@ -57,6 +82,8 @@ class Program
     static int SumBeforeIndex(int[] arr, int index)
     {
         int sumBeforeIndex = 0;
+        
+        //This for loop sums the elements before the start index
         for (int i = 0; i < index; i++)
         {
             sumBeforeIndex += arr[i];
@@ -67,10 +94,22 @@ class Program
     static int SumAfterIndex(int[] arr, int index)
     {
         int sumAfterIndex = 0;
+        
+        //This for loop sums the elements after the end index
         for (int i = index + 1; i < arr.Length; i++)
         {
             sumAfterIndex += arr[i];
         }
         return sumAfterIndex;
+    }
+
+    static void WriteArray(Array array)
+    {
+        Console.Write("Array elements: ");
+        foreach (int number in array)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
     }
 }
--- project3/ConsoleApp2/Program.cs
+++ project3/ConsoleApp2/Program.cs
@@ -5,9 +5,13 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine("This program accepts only english letters");
+        Console.WriteLine("-------------------------------");
+        
         Console.Write("Enter a string: ");
         string text = Console.ReadLine();
-        
+
+        //This while loop checks if the text is empty or null
         while (text == "" || text == null)
         {
             Console.WriteLine("Invalid text");
@@ -16,12 +20,15 @@ class Program
         }
         
         bool newOperation = false;
+        bool findStatus = false;
+        
+        //This while loop checks if the user wants to continue
         while(newOperation != true)
         {
             Console.WriteLine("Choose an operation: ");
-            Console.WriteLine("1 --> İşlem 1");
-            Console.WriteLine("2 --> İşlem 2");
-            Console.WriteLine("3 --> İşlem 3");
+            Console.WriteLine("1 --> Search words in text");
+            Console.WriteLine("2 --> Search words in text with substring");
+            Console.WriteLine("3 --> Draw letter count graphic");
             Console.WriteLine("4 --> Exit");
             int choice = int.Parse(Console.ReadLine());
             string searchText = "";
@@ -30,27 +37,90 @@ class Program
             {
                 case 1:
                     searchText = TakeSearchText();
+                    WriteInfo(text, searchText);
                     Console.WriteLine("-------------------------------");
 
                     int index = -1;
-                    bool findStatus = false;
+                    
+                    //This while loop finds the index of the search text in the text
                     while ((index = text.IndexOf(searchText, index + 1)) != -1)
                     {
-                        Console.WriteLine($"'{searchText}' in {index}. indeks");
+                        Console.WriteLine($"'{searchText}' in {index}. index");
                         findStatus = true;
                     }
 
+                    //This if statement checks if the search text is not found
                     if (findStatus != true)
                     {
                         Console.WriteLine($"'{searchText}' not found");
                     } 
+                    
+                    Console.WriteLine();
+
                     break;
 
                 case 2:
                     searchText = TakeSearchText();
+                    WriteInfo(text, searchText);
+                    Console.WriteLine("-------------------------------");
+                    
+                    int searchTextLength = searchText.Length;
+                    
+                    //This for loop finds the index of the search text in the text
+                    for (int i = 0; i < text.Length - searchTextLength + 1; i++)
+                    {
+                        if (text.Substring(i,searchTextLength ) == searchText)
+                        {
+                            Console.WriteLine($"'{searchText}' in {i}. index");
+                            findStatus = true;
+
+                        }
+                    }
+
+                    //This if statement checks if the search text is not found
+                    if (findStatus != true)
+                    {
+                        Console.WriteLine($"'{searchText}' not found");
+
+                    }
+                    
+                    Console.WriteLine();
+                    
                     break;
 
                 case 3:
+                    WriteInfo(text, searchText);
+                    
+                    Console.WriteLine();
+                    Console.WriteLine("Letter Count Graphic");
+                    Console.WriteLine("-------------------------------");
+                    
+                    Dictionary<char, int> letterCount = new Dictionary<char, int>();
+
+                    //This for loop adds the letters to the dictionary
+                    for (char letter = 'a'; letter <= 'z'; letter++)
+                    {
+                        letterCount[letter] = 0;
+                    }
+                    
+                    //This foreach loop counts the letters in the text
+                    foreach (char letter in text.ToLower())
+                    {
+                        if (char.IsLetter(letter))
+                        {
+                            letterCount[letter]++;
+                        }
+                    }
+                    
+                    //This foreach loop prints the letters and their count
+                    foreach (KeyValuePair<char, int> letterSelected in letterCount)
+                    {
+                        char letter = letterSelected.Key;
+                        int count = letterSelected.Value;
+                        char asterisks = '*';
+                        Console.WriteLine($"{letter} letter: {count} --> {new string(asterisks, count)}");
+                    }
+                    Console.WriteLine();
                     break;
 
                 case 4:
@@ -68,6 +138,8 @@ class Program
     {
         Console.Write("Enter a string that you want to search: ");
         string searchText = Console.ReadLine();
+        
+        //This while loop checks if the search text is empty or null
         while (searchText == "" || searchText == null)
         {
             Console.WriteLine("Invalid text");
@@ -77,8 +149,9 @@ class Program
 
         return searchText;
     }
-
-    
-
-
+    static void WriteInfo(string text, string searchText)
+    {
+        Console.WriteLine($"Text is : {text}");
+        Console.WriteLine($"Search text is : {searchText}");
+    }
 }
